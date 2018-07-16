38af3bad28912976b22695a3fcdbfea94f7641cf
Valentin Bandelier <vmbandelier@gmail.com>
2018-07-16 21:50:43 +0200

    Test Setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,3 +1,5 @@
+import unittest
+
 from flask.cli import FlaskGroup
 
 from project import app, db
@@ -8,9 +10,20 @@ cli = FlaskGroup(app)
 
 @cli.command()
 def recreate_db():
+    """Recreate the database"""
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """Runs the test without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    results = unittest.TextTestRunner(verbosity=2).run(tests)
+    if results.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
