81a2e2a9e94acb9d39345ad94f30f952e82da7d5
Valentin Bandelier <vmbandelier@gmail.com>
2018-07-21 22:08:49 +0200

    React Form

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -3,25 +3,50 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: ''
     };
-  }
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  };
 
   componentDidMount() {
     this.getUsers();
-  }
+  };
 
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
     .then((res) => { this.setState({ users: res.data.data.users }); })
     .catch((err) => { console.log(err); });
-  }
+  };
+
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then((res) => {
+        this.getUsers();
+        this.setState({ username: '', email: '' })
+     })
+    .catch((err) => { console.log(err) });
+  };
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
 
   render() {
     return (
@@ -32,13 +57,20 @@ class App extends Component {
               <br/>
               <h1 className="title is-1 is-1">All Users</h1>
               <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
+              <hr/><br/>
               <UsersList users={this.state.users}/>
             </div>
           </div>
         </div>
       </section>
     )
-  }
+  };
 };
 
 ReactDOM.render(
