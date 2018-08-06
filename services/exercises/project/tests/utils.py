b162479866fe579c179b10d908a8ecb983a8ce63
Valentin Bandelier <vmbandelier@gmail.com>
2018-08-06 14:34:18 +0200

    Exercises Database

--- services/exercises/project/tests/utils.py
+++ services/exercises/project/tests/utils.py
@@ -4,11 +4,14 @@ from project.api.models import Exercise
 
 def add_exercise(
         body=('Define a function called sum that takes two integers as '
-              'arguments and return their sum'
-        ),
+              'arguments and returns their sum'),
         test_code='sum(2, 2)',
         test_code_solution='4'):
-    exercise = Exercise(body, test_code, test_code_solution)
+    exercise = Exercise(
+        body=body,
+        test_code=test_code,
+        test_code_solution=test_code_solution,
+    )
     db.session.add(exercise)
     db.session.commit()
     return exercise
